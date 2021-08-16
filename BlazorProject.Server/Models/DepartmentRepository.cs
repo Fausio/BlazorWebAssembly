ad7efb4cef1921e8f46fffab39c2e2d00abea13b
fausio luis <fausioluis@live.com>
2021-08-16 23:03:33 +0200

    DepartmentRepository

---
+++ BlazorProject.Server/Models/DepartmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using BlazorProject.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Server.Models
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext db;
+
+        public DepartmentRepository(AppDbContext appDbContext)
+        {
+            this.db = appDbContext;
+        }
+        public async Task<Department> GetDepartmentById(int departmentId) => await db.Department.FirstOrDefaultAsync(d => d.DepartmentId == departmentId);
+
+        public async Task<IEnumerable<Department>> GetDepartments() => await db.Department.ToListAsync();
+    }
+}
