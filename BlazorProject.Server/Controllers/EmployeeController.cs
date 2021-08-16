b0f81d654f494986e45d155a209a99b16fadce06
fausio luis <fausioluis@live.com>
2021-08-16 23:24:21 +0200

    [Route("api/[controller]")]

--- BlazorProject.Server/Controllers/EmployeeController.cs
+++ BlazorProject.Server/Controllers/EmployeeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace BlazorProject.Server.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class EmployeeController : ControllerBase
     {
         public IActionResult Index()
